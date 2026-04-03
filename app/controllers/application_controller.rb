5599ab5fba3fbffcecd57afbf003a1fa2b6d2eba
Jeroen Nijhof <jeroen@clearcodewizards.com>
2026-04-03 08:50:05 +0200

    Add styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,18 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  before_action :authenticate_user!
-  after_action :verify_authorized
+  before_action :authenticate_user!, unless: :devise_controller?
+  after_action :verify_authorized, unless: :devise_controller?
+
+  layout :set_layout
+
+  private
+
+  def set_layout
+    if user_signed_in?
+      "application"
+    else
+      "devise"
+    end
+  end
 end
